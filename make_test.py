6dbd2a912131cc2f12e63b2c0dc9cfb1a9e82ed2
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2018-11-14 11:19:08 +0000

    Updated make_test.py to allow specifying a shebang.

--- make_test.py
+++ make_test.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2016 Vinay Sajip.
+# Copyright (C) 2018 Vinay Sajip.
 #
+import argparse
 import io
 import os
 import sys
@@ -14,11 +15,16 @@ print(sys.argv)
 '''
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--shebang', help='Specify shebang to add')
+    options = parser.parse_args()
     script_data = MAIN.strip().encode('utf-8')
     archive_data = io.BytesIO()
     with zipfile.ZipFile(archive_data, 'w', zipfile.ZIP_DEFLATED) as zf:
         zf.writestr('__main__.py', script_data)
-    shebang = b'#!/usr/bin/env python3 -u\n'
+    if options.shebang is None:
+        options.shebang = '/usr/bin/env python3 -u'
+    shebang = ('#!%s\n' % options.shebang).encode('utf-8')
     with open('x64/Debug/CLISimpleLauncher.exe', 'rb') as f:
         launcher_data = f.read()
     data = launcher_data + shebang + archive_data.getvalue()
